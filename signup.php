0a57741aa3fbededded7d263518b3aaff7ba0ba5
Harsh Upadhyay <hupadhyay1005@gmail.com>
2021-06-25 02:06:48 +0530

    implemented login part

--- signup.php
+++ signup.php
@@ -1,30 +1,25 @@
-<?php
-// database connection code
-if(isset($_POST['name']))
-{
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','db_contact');
 
-// get the post records
+<?php 
+    include('connection.php');  
+    $name = $_POST['name'];  
+	 $email = $_POST['email'];  
+    $phone = $_POST['phone']; 
+    $password = $_POST['password'];  
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$password = $_POST['password'];
-
+		
 // database insert SQL code
-$sql = "INSERT INTO `signup` VALUES ('$name', '$email', '$phone', '$password')";
+$sql = "INSERT INTO signup VALUES ('$name', '$email', '$phone', '$password')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
+
 if($rs)
 {
 	echo "Signed Up Successfully";
 }
-}
+
 else
 {
 	echo "Are you a genuine visitor?";
 	
-}
-?>
+}?>  
