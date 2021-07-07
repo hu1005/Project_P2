60d40f75b19317138c2878e41d8e99a681a2230d
Harsh Upadhyay <hupadhyay1005@gmail.com>
2021-07-08 00:24:30 +0530

    added the switch theme button and the pincode testing functionality

--- test.js
+++ test.js
@@ -1,32 +1,12 @@
+   document.logOn.onsubmit=validate;
 
-var mysql = require('mysql');
+   function validate(){
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "100500",
-  database: "sample"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-console.log("connected");
-});
-
-var firstName = document.getElementById("firstName");
-var lastName = document.getElementById("lastName");
-var psw = document.getElementById("psw");
-var userName = document.getElementById("userName");
-var email = document.getElementById("inputText");
-
-
-
-var sql = "INSERT INTO accounts (UserName, FirstName, LastName, Email, UserPassword) VALUES ('"+userName+ "', '"+firstName+"','"+lastName+"','"+email+"','"+psw+"')";
-con.query(sql, function (err, result) {
-    if (err) {
-        throw err;
-
-    }
-
-    console.log(result.affectedRows + " record(s) updated");
-  });
+var name=document.logOn.pw.value;
+    if(!name = 000000){              
+alert("Your Password Cant Have any thing other than a-zA-Z0-9!@#$%^*_| - Play It Straight!");
+    return false;
+}               
+alert("Your Password Cant Have any thing other than a-zA-Z0-9!@#$%^*_| - Play It Straight!");
+    return true;
+}
