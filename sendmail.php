2357e605480d505073f6b0f3b9c51e3d611fe1f5
Harsh Upadhyay <hupadhyay1005@gmail.com>
2021-07-13 14:16:04 +0530

    pincode popup, theme switching

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,19 @@
 <?php
+$name = $_POST['c_name'];
+$phone = $_POST['c_phone'];
+$email= $_POST['c_email'];
+$subject = $_POST['c_subject'];  
+$message = $_POST['c_message'];
+  
 $to_email = "hupadhyay2012@gmail.com";
-$subject = "Simple Email Test via PHP";
-$body = "Hi, This is test email send by PHP Script";
+$subject = $_POST['c_subject']; 
+$body = "Hi, My name is $name \r\n and my query is : $message \r\n My Contact Details are: \r\n email : $email \r\n $phone";
 $headers = "From: hupadhyay2012@gmail.com";
 
 if (mail($to_email, $subject, $body, $headers)) {
-    echo "Email successfully sent to $to_email...";
+    echo "$name, We have received your query.";
+	
+	echo "One of our representatives will contact you shortly";
 } else {
     echo "Email sending failed...";
 }
