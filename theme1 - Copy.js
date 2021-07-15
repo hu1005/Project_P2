b3d66a3563b82d6169189c6593ec2b97c3bc9a7c
Harsh Upadhyay <hupadhyay1005@gmail.com>
2021-07-15 23:37:07 +0530

    manny updations of images, css files, index 2 pages

---
+++ theme1 - Copy.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+		
+            var theme = document.getElementsByTagName('link')[1];
+  
+            if (theme.getAttribute('href') == '../style.css') {
+                theme.setAttribute('href', '../style1.css');
+            } else {
+                theme.setAttribute('href', '../style.css');
+            }
+        }
