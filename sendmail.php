0ae27299ae39cab06896ed3a10cf1edad50dff8d
Harsh Upadhyay <hupadhyay1005@gmail.com>
2021-07-16 01:15:28 +0530

    correctections in the signup form and the forgot password link

--- sendmail.php
+++ sendmail.php
@@ -7,11 +7,11 @@ $message = $_POST['c_message'];
   
 $to_email = "hupadhyay2012@gmail.com";
 $subject = $_POST['c_subject']; 
-$body = "Hi, My name is $name \r\n and my query is : $message \r\n My Contact Details are: \r\n email : $email \r\n $phone";
+$body = "Hi, My name is $name and my query is : $message My Contact Details are:- Email Address: $email Phone Number:- $phone";
 $headers = "From: hupadhyay2012@gmail.com";
 
 if (mail($to_email, $subject, $body, $headers)) {
-    echo "$name, We have received your query.";
+    echo "$name, We have received your query.".PHP_EOL;
 	
 	echo "One of our representatives will contact you shortly";
 } else {
